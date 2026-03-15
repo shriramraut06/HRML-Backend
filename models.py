6f0f3dae00bc92e5f0a206233588b220f236a224
Shriram Raut <144915494+shriramraut06@users.noreply.github.com>
2026-03-15 15:59:42 +0530

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from database import Base
 class Employee(Base):
     __tablename__ = "employees"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String, primary_key=True, index=True)
     employee_id = Column(String, unique=True, index=True, nullable=False)
     full_name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
